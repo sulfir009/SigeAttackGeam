edec9e38dc88df87fd8a07bf7324830368a4dc5e
sulfir009 <74712238+sulfir009@users.noreply.github.com>
2023-07-02 17:55:51 +0300

    NewMobs
    В папке mobs

---
+++ Assets/scripts/Battle/Battle/WaveEnemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveEnemy : MonoBehaviour
+{
+    //спавн мобов
+    public GameObject enemyPrefab; //модель персонажа
+    public int spawnEnemyCount = 1; // кількість ворогів
+    public List<Transform> spawner; // спавнери
+    
+    public float exctraTimeSpawn = 10f; //час досрочного спавна юнітів
+    private int enemyCount;
+    //таймер
+    public Text timerText;
+
+    public int waveNumber = 1;
+    public MobStrengthController mobStrengthController;
+
+    void Start()
+    {
+        SpawnEnemyWave(spawnEnemyCount);
+        timerText.text = "Час до нової хвилі: " + exctraTimeSpawn.ToString();
+    }
+
+    void Update()
+    {
+        //кількість об'єктів з тегом Ворог
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Enemy");
+        enemyCount = obj.Length;
+        //рахується час до досрочного спавна
+        exctraTimeSpawn -= Time.deltaTime;
+        timerText.text = "Час до нової хвилі: " + Mathf.Round(exctraTimeSpawn).ToString();
+        //якщо ворогів немає, або час закінчився
+        if (enemyCount == 0 || exctraTimeSpawn < 0)
+        {
+            
+            //збільшення сили ворогів
+            waveNumber++;
+            mobStrengthController.SetStrength(waveNumber * 5);
+
+            SpawnEnemyWave(spawnEnemyCount);
+            //повертаємо час для досрочного спавну
+            exctraTimeSpawn = 10f;
+        }
+
+    }
+    //позиція спавну
+    private Vector3 GeneralSpawnPosition(int indexSpawner)
+    {
+
+        float spawnPosX = Random.Range(-10, 10);
+        float spawnPosZ = Random.Range(-10, 10);
+
+        Vector3 randomPos = new Vector3(spawner[indexSpawner].transform.position.x + spawnPosX,
+            1, spawner[indexSpawner].transform.position.z + spawnPosZ);
+
+        return randomPos;
+
+    }
+    //спавн ворогів
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+
+        for (int j = 0; j < spawner.Count; j++)
+        {
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                Instantiate(enemyPrefab, GeneralSpawnPosition(i), Quaternion.identity);
+            }
+        }
+    }
+}
